fe16d72f9f992c78db19aee097200eb8a3608f45
BaranBaranDev <auniversty@gmail.com>
2025-06-24 18:24:05 +0300

    feat(voting): Initialize poll contract with basic functionality

--- voting-dapp/anchor/tests/votingdapp.test.ts
+++ voting-dapp/anchor/tests/votingdapp.test.ts
@@ -1,8 +1,40 @@
-  const IDL = require('../../target/idl/votingdapp.json')
+import { BankrunProvider, startAnchor } from "anchor-bankrun";
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { AnchorError, BN, Program } from "@coral-xyz/anchor";
+import { Votingdapp } from "../target/types/votingdapp";
+import { publicKey } from "@coral-xyz/anchor/dist/cjs/utils";
+  
+const IDL = require("../target/idl/votingdapp.json");
+const votingAdress = new PublicKey("JAVuBXeBZqXNtS73azhBDAoYaaAFfo4gWXoZe2e7Jf8H");
 
-describe('votingdapp', () => {
-  it('Initialize Votingdapp', async () => {
-    const context = await 
-  })
+describe("votingdapp", () => {
 
-})
+  it("Initializes the program", async () => {
+    const context = await startAnchor("",[{
+      name: "votingdapp",
+      programId: votingAdress,
+    }],[]);
+
+    const provider = new BankrunProvider(context);
+    const votingProgram  = new Program<Votingdapp>(IDL, provider);
+    await votingProgram.methods.initialize(
+      new BN(1),
+      "What is your favorite color?", 
+      new BN(0),
+      new BN(1850775545),
+    ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new BN(1).toArrayLike(Buffer, "le", 8)],
+      votingAdress
+    );
+
+    const poll  = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.pollId.toNumber()).toBe(1);
+    expect(poll.description).toEqual("What is your favorite color?");
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+  });
+});
